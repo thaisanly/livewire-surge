1a5c95ec69687b1dadbcb342eb71e077cd0f429c
Caleb Porzio <calebporzio@gmail.com>
2020-10-06 15:36:51 -0400

    Ep2

--- app/Http/Livewire/Dashboard.php
+++ app/Http/Livewire/Dashboard.php
@@ -4,13 +4,16 @@ namespace App\Http\Livewire;
 
 use App\Models\Transaction;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Dashboard extends Component
 {
+    use WithPagination;
+
     public function render()
     {
         return view('livewire.dashboard', [
-            'transactions' => Transaction::all(),
+            'transactions' => Transaction::paginate(10),
         ]);
     }
 }
