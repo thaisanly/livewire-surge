ddb73febcbcab6b45fda6bd25e119a810650d152
Caleb Porzio <calebporzio@gmail.com>
2020-10-23 11:40:47 -0400

    Add "hidden" to template tag

--- resources/views/components/notification.blade.php
+++ resources/views/components/notification.blade.php
@@ -18,7 +18,7 @@
     @notify.window="let message = $event.detail; messages.push(message); setTimeout(() => { remove(message) }, 2500)"
     class="fixed inset-0 flex flex-col items-end justify-center px-4 py-6 pointer-events-none sm:p-6 sm:justify-start space-y-4"
 >
-    <template x-for="(message, messageIndex) in messages" :key="messageIndex">
+    <template x-for="(message, messageIndex) in messages" :key="messageIndex" hidden>
         <div
             x-transition:enter="transform ease-out duration-300 transition"
             x-transition:enter-start="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
